150866d075b9c971511797ec77105e3248428676
ngadiono <ngadiono@djavaui.com>
2018-08-27 00:58:49 +0700

    create class component

--- ageteller/src/index.js
+++ ageteller/src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import App from './components/App';
+
 ReactDOM.render(
-  <div>ageteller</div>, document.getElementById('root')
+  <App />, document.getElementById('root')
 )
